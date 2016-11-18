52c2d9a7145ce064c4368c91c2d4064a8017de57
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-11-18 12:24:49 -0500

    Updated setup sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,9 @@
--- create todos
+/* Drop everything */
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
 
-CREATE TABLE todos (
+/* create todos */
+CREATE TABLE IF NOT EXISTS todos (
   todo_id BIGSERIAL    NOT NULL,
   todo    VARCHAR(255) NOT NULL
 );
